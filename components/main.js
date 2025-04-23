ee7c50ebad1c3745f0d43bb345efe84798b1efd8
SRSerhii <phantom3777@gmail.com>
2025-04-23 13:06:27 +0300

    added button hover color

--- components/main.js
+++ components/main.js
@@ -2,29 +2,22 @@ import { useState } from "react";
 const Moralis = require('moralis').default;
 const { EvmChain } = require('@moralisweb3/common-evm-utils');
 import Select from "react-select";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/landing.module.css";
+
+import Image from "next/image";
+import btcImg from "../public/assets/BTC_img.png";
+import btcEllipse from "../public/assets/Ellipse 8.png";
 
 export default function Header() {
+
   const [showResult, setShowResult] = useState(false);
   const [result, setResult] = useState("");
-  const [chainValue, setChainValue] = useState("");
-
-  const customStyles = {
-    option: (provided) => ({
-      ...provided,
-      color: "#000000",
-      backgroundColor: "#ffffff",
-    }),
-  };
-
-  const changeHandler = (chainValue) => {
-    setChainValue(chainValue);
-  };
 
   const handleSubmit = async () => {
     const token_address = document.querySelector("#contractAddress").value;
     if (!token_address) {
       setResult("Please enter a token address.");
+      setShowResult(true);
       return;
     }
 
@@ -36,51 +29,62 @@ export default function Header() {
         },
         body: JSON.stringify({ address: token_address }),
       });
-      console.log(response);
-      if (!response.ok) {
-        throw new Error("Fetch Post Error");
-      }
+
+      if (!response.ok) throw new Error("Fetch Post Error");
+
       const json = await response.json();
-      console.log(json);
       setResult(`$ ${json.price}`);
       setShowResult(true);
-      setChainValue("");
       document.querySelector("#contractAddress").value = "";
     } catch (error) {
       console.error("Error fetching token price:", error);
+      setResult("Failed to fetch token price.");
+      setShowResult(true);
     }
-
   };
 
   return (
-    <section className={styles.main}>
-      <form
-        className={styles.getTokenForm}
-        name="create-profile-form"
-        method="POST"
-        action="#"
-      >
-        <label className={styles.label} htmlFor="contractAddress">
-          Add Any Contract Address in any of these chains: Ethereum, Bsc, Polygon, Avalanche, Arbitrum, Base, Optimism, Linea, Solana
-        </label>
-        <input
-          className={styles.contractAddress}
-          type="text"
-          id="contractAddress"
-          name="contractAddress"
-          placeholder="e.g. 0x6982508145454ce325ddbe47a25d4ec3d2311933"
-          maxLength="120"
-          required
-        />
-          <div className={styles.label}>
-          Hint: You can get a token address on CoinMarketCap.com or similar website.</div>
-      </form>
-      <button className={styles.form_btn} onClick={handleSubmit}>
-        Submit
-      </button>
-      <section className={styles.result}>
-        {showResult && <p>{result}</p>}
+    <main className={styles.wrapper}>
+
+      {/* Hero section */}
+      <section className={styles.hero}>
+        <h1>Check the price for any Contract Address in any of these chains: <br />Ethereum, Bsc, Polygon, Avalanche, Arbitrum, Base, Optimism, Linea, Solana.</h1>
+
       </section>
-    </section>
+
+
+      {/* Token price checker */}
+      <section className={styles.tokenPriceSection}>
+
+        <h2>Enter Token Contract Address to get the price</h2>
+        <input
+          id="contractAddress"
+          className={styles.tokenInput}
+          type="text"
+          placeholder="e.g. 0x6982508145454ce325ddbe47a25d4ec3d2311933"
+        />
+        <button onClick={handleSubmit} className={styles.primaryBtn}>
+          Get Price
+        </button>
+
+        {showResult && (
+          <div className={styles.tokenPriceBox}>
+            <span>{result}</span>
+          </div>
+        )}
+      </section>
+      <div className={styles.imageWrapper}>
+        <Image
+          src={btcEllipse}
+          alt="ellipse"
+          className={styles.baseImage}
+        />
+        <Image
+          src={btcImg}
+          alt="BTC img"
+          className={styles.overlayImage}
+        />
+      </div>
+    </main>
   );
 }
